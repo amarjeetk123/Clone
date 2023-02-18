1a817606ffb1d01af277338cdcd737b18afcee27
amarjeetk123 <ak123del@gmail.com>
2023-02-19 00:01:48 +0530

    all the sections is comlreted

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 let box = document.getElementById("box")
+let cross = document.getElementById("cross")
+let three_line = document.getElementById("three-line")
+cross.style.display = 'none'
+
+let nav_list = document.getElementById("nav-list")
 box.style.display = 'none'
 let downarrow = document.getElementById("down-arrow")
 downarrow.addEventListener("click" , () => {
@@ -9,3 +14,25 @@ downarrow.addEventListener("click" , () => {
     box.style.display = 'none'
   }
 } )
+cross.addEventListener("click" , () => {
+  if(cross.style.display !== 'none'){
+    cross.style.display = 'none'
+    nav_list.style.left = '100%'
+  }
+  else{
+    box.style.display = 'none'
+    
+  }
+  nav_list.style.left = '100%'
+} )
+three_line.addEventListener("click" , () => {
+  if(cross.style.display === 'none'){
+    cross.style.display = 'block'
+    nav_list.style.left = '60%'
+  }
+  else{
+    cross.style.display = 'none'
+    
+  }
+} )
+
